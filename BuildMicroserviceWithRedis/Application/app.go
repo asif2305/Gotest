2460ce1d02f15d7daadd5c250c407abed7d7bd50
Asif Ahmed <asif.ahmed@campusgenius.com>
2024-03-29 13:13:58 +0100

    update

--- BuildMicroserviceWithRedis/Application/app.go
+++ BuildMicroserviceWithRedis/Application/app.go
@@ -25,7 +25,7 @@ func (a *App) Start(ctx context.Context) error {
 	err := server.ListenAndServe()
 
 	if err != nil {
-		return fmt.Errorf("failed to start server: %w", err)
+		return fmt.Errorf("failed to start server : %w", err)
 	}
 	return nil
 }
