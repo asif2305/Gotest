e0cd43b752d0070e93317a6196f4ac268fe19fa9
Asif Ahmed <asif.ahmed@campusgenius.com>
2024-03-29 01:01:12 +0100

    Build a Microservice with Go  - Routes & Handlers

--- BuildMicroserviceWithRedis/main.go
+++ BuildMicroserviceWithRedis/main.go
@@ -1,33 +1,18 @@
 package main
 
 import (
+	application "GO/BuildMicroserviceWithRedis/Application"
+	"context"
 	"fmt"
-	"net/http"
-
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
 )
 
 // Package chi is a small, idiomatic and composable router for building HTTP services .
 func main() {
+	app := application.New()
 
-	router := chi.NewRouter()
-	router.Use(middleware.Logger)
-
-	router.Get("/hello", basicHandler)
-
-	server := &http.Server{
-		Addr:    ":3000",
-		Handler: router,
-	}
-
-	err := server.ListenAndServe()
+	err := app.Start(context.TODO())
 	if err != nil {
-		fmt.Println("failed to listen to server", err)
+		fmt.Println("failed to start app:", err)
 	}
 
-}
-
-func basicHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello World"))
 }
