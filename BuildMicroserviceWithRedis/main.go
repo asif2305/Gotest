1c11dc5216d7ba74049b73eace46561eade7826a
Asif Ahmed <asif.ahmed@campusgenius.com>
2024-12-10 01:26:27 +0100

    updated

--- BuildMicroserviceWithRedis/main.go
+++ BuildMicroserviceWithRedis/main.go
@@ -4,13 +4,17 @@ import (
 	application "GOTEST/BuildMicroserviceWithRedis/Application"
 	"context"
 	"fmt"
+	"os"
+	"os/signal"
 )
 
 // Package chi is a small, idiomatic and composable router for building HTTP services .
 func main() {
 	app := application.New()
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
 
-	err := app.Start(context.TODO())
+	err := app.Start(ctx)
 	if err != nil {
 		fmt.Println("failed to start app:", err)
 	}
